16db8582d1b2d910a703a5e298d64b302bbb400a
lichengjin <lichengjin606@gmail.com>
2018-03-20 16:41:23 +0800

    up

---
+++ data_helpers.py
@@ -0,0 +1,111 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+Created on 2017年06月21日
+
+@author: lichengjin
+"""
+from __future__ import absolute_import
+import os
+import math
+import random
+import argparse
+import numpy as np
+import tensorflow as tf
+
+SEPARATOR = ','
+
+
+def get_config_args():
+    parser = argparse.ArgumentParser(description='knrm train argument')
+    parser.add_argument('--train-data-path', type=str, default='./data/train.pairs.hashed.shuf',
+                        help='the path that keeps the train data file')
+    parser.add_argument('--test-data-path', type=str, default='./data/dev.pairs.hashed.shuf',
+                        help='the path that keeps the test data file')
+    parser.add_argument('--model-dir', type=str, default='./model', help='the directory that keeps the model file')
+    parser.add_argument('--activation', type=str, default='relu', help='activation function')
+    parser.add_argument('--optimizer', type=str, default='grad', help='optimizer')
+    parser.add_argument('--num-epochs', type=int, default=10, help='the number of training epochs')
+    parser.add_argument('--batch-size', type=int, default=3, help='the batch size')
+    parser.add_argument('--embedding-dim', type=int, default=200, help='Dimensionality of word embedding')
+    parser.add_argument('--vocabulary-size', type=int, default=2000000, help='Size of vocabulary')
+    parser.add_argument('--learning-rate', type=float, default=0.1, help='the learning rate')
+    parser.add_argument('--max-query-term-length', type=int, default=20, help='the max query term length')
+    parser.add_argument('--max-doc-term-length', type=int, default=20, help='the max doc term length')
+    parser.add_argument('--kernel-num', type=int, default=11,
+                        help='the number of kernels, default: 11. One exact match kernel and 10 soft kernels')
+    parser.add_argument('--lamb', type=float, default=0.5,
+                        help='use to the gaussian kernels sigma value, sigma = lamb * bin_size')
+    parser.add_argument('--init-scale', type=float, default=0.1, help='init scale')
+    parser.add_argument('--show-freq', type=int, default=10, help='Show train results after this many steps')
+    parser.add_argument('--test-freq', type=int, default=10, help='Test model results after this many steps')
+    parser.add_argument('--save-freq', type=int, default=10, help='save model each number steps')
+    parser.add_argument('--test-num', type=int, default=10000, help='Test sample size')
+
+    config = parser.parse_args()
+    return config
+
+
+def batch_iter(data, batch_size):
+    """
+    Generates data batch iterator for data set.
+    """
+    data_size = len(data)
+    num_batches_per_epoch = int(math.ceil(float(len(data))/batch_size))
+    for batch_num in range(num_batches_per_epoch):
+        start_index = batch_num * batch_size
+        end_index = min((batch_num + 1) * batch_size, data_size)
+        yield data[start_index:end_index]
+
+
+def load_all_data_at_once(data_path, max_query_term_length, max_doc_term_length, max_num=None):
+    """
+    Load train data，when the amount of data is small(less than 100G), select this method
+    """
+    if not os.path.exists(data_path):
+        print ("%s is not exist!" % data_path)
+        exit(0)
+    else:
+        print ("%s is exist!" % data_path)
+    test_data = np.loadtxt(data_path, dtype=np.int32, delimiter=SEPARATOR)
+    if max_num:
+        # 设置随机数种子
+        np.random.seed(10)
+        # 返回一个随机后的的数组,并取前max_num个
+        shuffle_indices = np.random.permutation(np.arange(len(test_data)))
+        test_data = test_data[shuffle_indices][:max_num]
+
+    test_data_split = np.split(test_data, [max_query_term_length, max_query_term_length + max_doc_term_length], axis=1)
+    query_term_ids = test_data_split[0]
+    pos_doc_term_ids = test_data_split[1]
+    neg_doc_term_ids = test_data_split[2]
+    doc_term_ids = np.append(pos_doc_term_ids[:, np.newaxis], neg_doc_term_ids[:, np.newaxis], 1)
+    print("query_term_ids shape: {:s}".format(query_term_ids.shape))
+    print("doc_term_ids shape: {:s}".format(doc_term_ids.shape))
+    return query_term_ids, doc_term_ids
+
+
+def load_batch_data_by_queue(filename_list, max_query_term_length, max_doc_term_length,
+                             num_epochs=10, batch_size=256, num_threads=12):
+    """
+    Load batch data by queue，when the amount of data is big(more than 100G), select this method
+    """
+    for filename in filename_list:
+        if not tf.gfile.Exists(filename):
+            raise ValueError('Failed to find file: ' + filename)
+    filename_queue = tf.train.string_input_producer(filename_list, num_epochs=num_epochs)
+    reader = tf.TextLineReader()
+    _, records = reader.read_up_to(filename_queue, batch_size)
+    record_defaults = [tf.constant([], dtype=tf.int32)] * (max_query_term_length + 2 * max_doc_term_length)
+    temp_tensor = tf.stack(tf.decode_csv(records, record_defaults, SEPARATOR), 1)
+    split_size = [max_query_term_length] + [max_doc_term_length] * 2
+    query_term_ids, pos_doc_term_ids, neg_doc_term_ids = tf.split(temp_tensor, split_size, 1)
+    doc_term_ids = tf.concat([tf.expand_dims(pos_doc_term_ids, 1), tf.expand_dims(neg_doc_term_ids, 1)], axis=1)
+    batch_data = tf.train.shuffle_batch((query_term_ids, doc_term_ids),
+                                        batch_size=batch_size,
+                                        capacity=batch_size*200,
+                                        min_after_dequeue=batch_size*10,
+                                        num_threads=num_threads,
+                                        enqueue_many=True)
+
+    return batch_data
---
+++ knrm.py
@@ -0,0 +1,166 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+Created on 2017年07月06日
+
+@author: lichengjin
+"""
+import numpy as np
+import tensorflow as tf
+
+
+def print_variable_info(var):
+    """
+    print variable info
+    """
+    print (var.op.name, ' ', var.get_shape().as_list())
+
+
+class KNRMModel(object):
+    """
+    K-NRM：a kernel based neural ranking model
+    """
+    def __init__(self, input_query, input_doc, config, scope):
+        with tf.name_scope('input'):
+            print_variable_info(input_query)
+            print_variable_info(input_doc)
+            # num_per_entry在train过程中要设置成2，test过程大于等于1即可，方便一次处理 一个query与多个doc的score
+            num_per_entry = input_doc.get_shape()[1].value
+            self.learning_rate = config.learning_rate
+            self.max_query_term_length = config.max_query_term_length
+            self.max_doc_term_length = config.max_doc_term_length
+            self.kernel_num = config.kernel_num
+            self.init_scale = config.init_scale
+            self.vocabulary_size = config.vocabulary_size
+            self.embedding_dim = config.embedding_dim
+            if 'relu' == config.activation:
+                self.activation = tf.nn.relu
+            else:
+                self.activation = tf.nn.tanh
+            if 'adam' == config.optimizer:
+                self.optimizer = tf.train.AdamOptimizer
+            else:
+                self.optimizer = tf.train.GradientDescentOptimizer
+
+        with tf.name_scope('embedding'):
+            # look up embeddings for each term.
+            self.embedding_weight = tf.get_variable(
+                'embedding_weight',
+                shape=[self.vocabulary_size + 1, self.embedding_dim],
+                initializer=tf.random_uniform_initializer(-1 * self.init_scale, 1 * self.init_scale))
+            # query_embedded, [batch_size, max_query_term_length, embedding_dim]
+            self.query_embedded = tf.nn.embedding_lookup(self.embedding_weight, input_query, name='query_embedded')
+            print_variable_info(self.query_embedded)
+            # doc_embedded, [batch_size, num_per_entry, max_doc_term_length, embedding_dim]
+            self.doc_embedded = tf.nn.embedding_lookup(self.embedding_weight, input_doc, name='doc_embedded')
+            print_variable_info(self.doc_embedded)
+
+        with tf.name_scope('translation'):
+            # normalize and compute translation matrix.
+            norm_query = tf.sqrt(tf.reduce_sum(tf.square(self.query_embedded), 2, keep_dims=True))
+            self.query_embedded_normalized = self.query_embedded / norm_query
+            norm_doc = tf.sqrt(tf.reduce_sum(tf.square(self.doc_embedded), 3, keep_dims=True))
+            doc_embedded_normalized = self.doc_embedded / norm_doc
+            self.doc_embedded_normalized = tf.reshape(doc_embedded_normalized,
+                                                      [-1, num_per_entry * self.max_doc_term_length,
+                                                       self.embedding_dim])
+            self.translation_matrix = tf.matmul(self.query_embedded_normalized,
+                                                self.doc_embedded_normalized,
+                                                transpose_b=True, name='translation_matrix')
+
+        with tf.name_scope("kernel_pooling"):
+            # Get the mu for each gaussian kernel
+            self.mu_list = self.kernel_mu(self.kernel_num)
+            # self.mu_list = tf.reshape(self.mu_list, shape=[1, 1, self.kernel_num])
+            self.lamb = config.lamb
+            # Get the sigma for each gaussian kernel
+            self.sigma_list = self.kernel_sigma(self.kernel_num, self.lamb)
+            # self.sigma_list = tf.reshape(self.sigma_list, shape=[1, 1, self.kernel_num])
+            # compute Gaussian scores of each kernel
+            tmp = tf.exp(-tf.square(tf.subtract(tf.expand_dims(self.translation_matrix, -1), self.mu_list)) / (2 * tf.square(self.sigma_list)))
+            tmp_reshape = tf.reshape(tmp, [-1, num_per_entry, self.max_query_term_length, self.max_doc_term_length, self.kernel_num])
+            # sum up gaussian scores
+            kde = tf.reduce_sum(tmp_reshape, [3])
+            # aggregated query terms，store the soft-TF features from each field.
+            soft_tf_feats = tf.reduce_sum(tf.log(tf.maximum(kde, 1e-10)) * 0.01, [2])  # 0.01 used to scale down the data.
+            # [batch, num_per_entry, n_bins]
+            print "batch feature shape:", soft_tf_feats.get_shape()
+            feats_flat = tf.reshape(soft_tf_feats, [-1, self.kernel_num])
+
+        # Learning-To-Rank layer.
+        with tf.name_scope("learning_to_rank"):
+            tmp = np.sqrt(6.0 / (self.kernel_num + 1))
+            self.weight = tf.get_variable(
+                'weight',
+                shape=[self.kernel_num, 1],
+                dtype=tf.float32,
+                initializer=tf.random_uniform_initializer(-1 * tmp, tmp)
+            )
+            self.bias = tf.get_variable(
+                'bias',
+                shape=[1],
+                dtype=tf.float32,
+                initializer=tf.constant_initializer(1.0)
+            )
+
+        with tf.name_scope("output"):
+            # scores is the final matching score.
+            scores = self.activation(tf.matmul(feats_flat, self.weight) + self.bias)
+            self.scores = tf.reshape(scores, [-1, num_per_entry])
+            print "scores: ", self.scores
+            self.pos_scores = tf.slice(self.scores, [0, 0], [-1, 1], name='pos_scores')
+            print "pos_scores: ", self.pos_scores
+            self.neg_scores = tf.slice(self.scores, [0, 1], [-1, -1], name='neg_scores')
+            print "neg_scores: ", self.neg_scores
+            # loss, max(0, 1 - score1 + score2)
+            self.loss = tf.reduce_mean(tf.maximum(0.0, 1 - self.pos_scores + self.neg_scores))
+            tf.summary.scalar("loss", self.loss)
+
+        self.global_step = tf.train.get_or_create_global_step()
+
+        optimizer = self.optimizer(self.learning_rate)
+        self.grads_and_vars = optimizer.compute_gradients(self.loss)
+        self.train_op = optimizer.apply_gradients(self.grads_and_vars, global_step=self.global_step)
+
+        grad_summaries = []
+        for grad, var in self.grads_and_vars:
+            if grad is not None:
+                grad_hist_summary = tf.summary.histogram("{}/grad/hist".format(var.name), grad)
+                sparsity_summary = tf.summary.scalar("{}/grad/sparsity".format(var.name), tf.nn.zero_fraction(grad))
+                grad_summaries.append(grad_hist_summary)
+                grad_summaries.append(sparsity_summary)
+        self.summary = tf.summary.merge(tf.get_collection(tf.GraphKeys.SUMMARIES, scope))
+
+    @staticmethod
+    def kernel_mu(kernel_num):
+        """
+        计算每个高斯核的均值, 均值设置为每个bin的中值
+        Get the mu for each gaussian kernel，mu is the middle of each bin
+        :param kernel_num: the number of kernels including exact match，first one is exact match
+        :return: mu_list, a list of mu. e.g.：[1, 0.9, 0.7, 0.5, 0.3, 0.1, -0.1, -0.3, -0.5, -0.7, -0.9]
+        """
+        mu_list = [1.0]  # for exact match
+        if kernel_num == 1:
+            return mu_list
+        bin_size = 2.0 / (kernel_num - 1)  # score range from [-1, 1]
+        mu_list.append(1.0 - bin_size / 2.0)  # mu: middle of the bin
+        for i in xrange(1, kernel_num - 1):
+            mu_list.append(mu_list[i] - bin_size)
+        print "kernel mu values: ", mu_list
+        return mu_list
+
+    @staticmethod
+    def kernel_sigma(kernel_num, lamb):
+        """
+        计算每个高斯核的标准差
+        :param kernel_num: the number of kernels including exact match，first one is exact match
+        :param lamb: use to the gaussian kernels sigma value, sigma = lamb * bin_size
+        :return: sigma_list, a list of simga. e.g.：[1e-5, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
+        """
+        sigma_list = [0.00001]  # for exact match. small variance -> exact match
+        if kernel_num == 1:
+            return sigma_list
+        bin_size = 2.0 / (kernel_num - 1)  # score range from [-1, 1]
+        sigma_list += [bin_size * lamb] * (kernel_num - 1)
+        print "kernel sigma values: ", sigma_list
+        return sigma_list
