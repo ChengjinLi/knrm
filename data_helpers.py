b2d5d58792fbc97a2220af7c18c522188769f5a9
lichengjin <lichengjin606@gmail.com>
2018-03-20 17:07:16 +0800

    up

--- data_helpers.py
+++ data_helpers.py
@@ -104,7 +104,7 @@ def load_batch_data_by_queue(filename_list, max_query_term_length, max_doc_term_
     record_defaults = [tf.constant([], dtype=tf.int32)] * (max_query_term_length + 5 * max_doc_term_length)
     temp_tensor = tf.stack(tf.decode_csv(records, record_defaults, SEPARATOR), 1)
     # split_size = [max_query_term_length] + [max_doc_term_length] * 2
-    split_size = [max_query_term_length] + [max_doc_term_length] * 5
+    split_size = [max_query_term_length] + [max_doc_term_length * 5]
     # query_term_ids, pos_doc_term_ids, neg_doc_term_ids = tf.split(temp_tensor, split_size, 1)
     query_term_ids, doc_term_ids = tf.split(temp_tensor, split_size, 1)
     doc_term_ids = tf.reshape(doc_term_ids, [-1, 5, max_doc_term_length])
